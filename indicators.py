b061ace54a5357490d7d6556a12429586159971f
Alexander Sotomayor <alex15spineda@hotmail.com>
2025-03-24 12:51:02 -0600

    added svm model

--- indicators.py
+++ indicators.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import numpy as np
 import ta
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report
+
 
 # Descarga de datos
 data = pd.read_csv('aapl_5m_test.csv').dropna()
@@ -11,19 +16,53 @@ williams = ta.momentum.WilliamsRIndicator(high=data['High'], low=data['Low'], cl
 
 dataset = data.copy()
 
+# Agregar indicadores técnicos al dataset
 dataset['RSI'] = rsi.rsi()
 dataset['Ultimate'] = ultimate.ultimate_oscillator()
 dataset['Williams'] = williams.williams_r()
 
-dataset['RSI_BUY'] = dataset['RSI'] < 15
-dataset['RSI_SELL'] = dataset['RSI'] > 75
-
-dataset['WILLIAMS_BUY'] = dataset['Williams'] < -80
-dataset['WILLIAMS_SELL'] = dataset['Williams'] > -20
-
-dataset['ULTIMATE_BUY'] = dataset['Ultimate'] < 30
-dataset['ULTIMATE_SELL'] = dataset['Ultimate'] > 70
-
 dataset = dataset.dropna()
 
-print(dataset.head())
+# Retornos de cada 2 horas
+dataset['future_return'] = dataset['Close'].shift(-24) / dataset['Close'] - 1
+
+# Establecer umbrales para las señales
+buy_threshold = 0.003  
+sell_threshold = -0.003
+
+def generate_signal(x):
+    if x > buy_threshold:
+        return 'BUY'
+    elif x < sell_threshold:
+        return 'SELL'
+    else:
+        return 'WAIT'
+
+dataset['signal'] = dataset['future_return'].apply(generate_signal)
+
+dataset['Datetime'] = pd.to_datetime(dataset['Datetime'])
+dataset.set_index('Datetime', inplace=True)
+
+# Seleccionar solo cada 2 horas
+dataset_2h = dataset.iloc[::24, :]
+
+features = ['RSI', 'Ultimate', 'Williams']
+X = dataset_2h[features]
+y = dataset_2h['signal']
+
+# Entrenar el modelo SVM
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
+
+# Escalado para mejor funcionamiento del modelo
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Entrenar SVM
+svm = SVC(kernel='rbf', C=1, gamma='scale')
+svm.fit(X_train_scaled, y_train)
+
+# Predicciones
+y_pred = svm.predict(X_test_scaled)
+print(classification_report(y_test, y_pred))
